ff20bec82084ab8a6bf4af417406d8a31050c769
Afsoon <aslani.a7260@gmail.com>
2025-10-05 12:04:43 +0330

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
 	server: {
 		host: '::',
 		port: 8081,
-		base: '/afsoonaslanii/',
+		base: '/',
 	},
 	plugins: [react(), mode === 'development' && componentTagger()].filter(
 		Boolean
